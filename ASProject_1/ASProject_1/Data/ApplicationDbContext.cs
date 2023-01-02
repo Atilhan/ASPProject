d15748918fec287ec1de52bb7ee5b66e7b1feeb9
DESKTOP-LP8HE8M\Atilhan <Gorinchem1995@Hotmail.com>
2023-01-02 17:27:22 +0100

    Trying to make a app on ASP

---
+++ ASProject_1/ASProject_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASProject_1.Models;
+
+namespace ASProject_1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASProject_1.Models.Joke> Joke { get; set; } = default!;
+    }
+}
